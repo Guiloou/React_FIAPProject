cc196f1d5e803fcd89549634723119ccb3acef3b
Guiloou <guilhermeloou@hotmail.com>
2023-11-20 06:14:57 -0300

    Codando em react para o projeto da FIAP

---
+++ src/Pages/Experiences/index.jsx
@@ -0,0 +1,13 @@
+import { Text, Title } from "../Formation/style"
+
+
+function Experiences() {
+  return (
+    <section>
+      <Title>Experiências:</Title>
+      <Text>Sistemas para internet - FIAP</Text>
+    </section>
+  )
+}
+
+export default Experiences
---
+++ src/Pages/Hobbies/index.jsx
@@ -0,0 +1,13 @@
+import { Text, Title } from "../Formation/style"
+
+
+function Hobbies() {
+  return (
+    <section>
+      <Title>Hobbies:</Title>
+      <Text>Sistemas para internet - FIAP</Text>
+    </section>
+  )
+}
+
+export default Hobbies
